d10678646048cb4189a351be324c3474a0bc30f0
Sameer Mangulkar <sameermangulkar@gmail.com>
2022-03-08 21:20:37 +0530

    product list/home page photos update

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,6 +59,26 @@ new Swiper(".clients-slider", {
   },
 });
 
+
+  /**
+   * Portfolio details slider
+   */
+  new Swiper('.portfolio-details-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+
+
 /**
  * Testimonials slider
  */
